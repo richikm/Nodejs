3c2e3f8757e881a9c174826973d2acb5e4f29525
richikm <69891493+richikm@users.noreply.github.com>
2020-12-09 14:22:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world!Version is Dev\n');
+  res.send('Hello world!Version is Test\n');
 });
 
 var port = process.env.PORT||PORT;
