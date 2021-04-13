9ed68e97949b99c56f47ff7a9a332de415ec688f
richikm <richikm@gmail.com>
2021-04-13 23:34:20 +0200

    Environment Prod

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world!Version is test\n');
+  res.send('Hello world!Version is Prod\n');
 });
 
 var port = process.env.PORT||PORT;
