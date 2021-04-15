7f1ae09b05b70b2a8026052023e04c384dc23114
richikm <richikm@gmail.com>
2021-04-15 17:38:55 +0200

    updated mesage

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello there !Version is Prod\n');
+  res.send('Hello world !Version is test\n');
 });
 
 var port = process.env.PORT||PORT;
